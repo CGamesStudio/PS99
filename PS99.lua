b5e468bb65e44106e87a5595a9b6870565c30691
CGamesStudio <151459809+CGamesStudio@users.noreply.github.com>
2024-02-25 21:34:43 +0100

    Update PS99.lua

--- PS99.lua
+++ PS99.lua
@@ -1,12 +1,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Chris Hub", "DarkTheme")
 
-if game.PlaceId == 8737899170 then
-    print("Correct Game")
-else
-    game.Players.LocalPlayer:Kick("Incorrect game")
-end
-
 -- localplayer
 
 local localplayer = Window:NewTab("LocalPlayer")
