6d5087f1027f809bee50de4aeea3e7b5c92cb614
CGamesStudio <151459809+CGamesStudio@users.noreply.github.com>
2024-02-25 21:22:56 +0100

    Update PS99.lua

--- PS99.lua
+++ PS99.lua
@@ -1,13 +1,8 @@
-local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
-local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
-local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Chris Hub", "DarkTheme")
 
-local Window = Fluent:CreateWindow({
-    Title = "Chris Hub " .. Fluent.Version,
-    SubTitle = "by someone that exists lol",
-    TabWidth = 160,
-    Size = UDim2.fromOffset(580, 460),
-    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
-    Theme = "Dark",
-    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
-})
+if game.PlaceId == 8737899170 then
+    print("Correct Game")
+else
+    game.Players.LocalPlayer:Kick("Incorrect game")
+end
