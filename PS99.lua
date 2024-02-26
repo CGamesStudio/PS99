a249f68dfab62445db8499a7b3ed400c95dd372d
CGamesStudio <151459809+CGamesStudio@users.noreply.github.com>
2024-02-26 21:34:21 +0100

    Update PS99.lua

--- PS99.lua
+++ PS99.lua
@@ -5,6 +5,41 @@ local Window = Library.CreateLib("Chris Hub", "DarkTheme")
 
 local localplayer = Window:NewTab("LocalPlayer")
 local lpsection = localplayer:NewSection("LocalPlayer")
-lpsection:NewSlider("Walkspeed", "SliderInfo", 500, 0, function(s)
+lpsection:NewSlider("Walkspeed", "Change your speed !", 500, 0, function(s)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+
+lpsection:NewSlider("JumpPower", "Jump higher !", 500, 0, function(s)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+end)
+
+lpsection:NewToggle("Noclip", '"What does collision mean ?"', function(state)
+    if state then
+            while task.wait() do
+                for _,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+                    v.CanCollide = false
+                end
+            end
+        else
+            while task.wait() do
+                for _,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+                    v.CanCollide = true
+                end
+            end
+        end
+    end)
+
+local othertab = Window:NewTab("Other Scripts")
+local otsection = othertab:NewSection("Other Scripts")
+
+otsection:NewButton("Infinite Yield", "Fe Admin", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+end)
+
+otsection:NewButton("Redz Hub", "OP Pet Simulator 99 Hub ! (Keyless)", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/PetSimulator99/main/redz9999.lua"))()
+end)
+
+otsection:NewButton("SkyHub", "OP Pet Simulator 99 Hub ! (Has a key system)", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/SKOIXLL/RIVERHUB-SKYHUB/main/WL.lua'))();
 end)
