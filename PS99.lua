d9e66bcea3607c50c124d1553dceace76a64c423
CGamesStudio <151459809+CGamesStudio@users.noreply.github.com>
2024-02-27 21:00:32 +0100

    Update PS99.lua

--- PS99.lua
+++ PS99.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Chris Hub", "DarkTheme")
+local Window = Library.CreateLib("Chris Hub [BETA]", "DarkTheme")
 
 -- Localplayer
 local localplayer = Window:NewTab("LocalPlayer")
